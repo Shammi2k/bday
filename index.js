80a303de9e27cb3c32e066b1abd27ff627b7f75c
Shammi <shammi2k@gmail.com>
2024-04-19 21:04:51 +0530

    Added captions

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 $(".timer").each(function (index, element) {
-    var countdownDate = new Date("Apr 20, 2024 00:00:00").getTime() + $(element).attr("hours") * 60 * 60 * 1000;
-    var counter = setInterval(() => {
-        var now = new Date().getTime();
-        var distance = countdownDate - now;
-        if (distance > 0) {
-            var hours = ("0" + Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).slice(-2);
-            var minutes = ("0" + Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).slice(-2);
-            var seconds = ("0" + Math.floor((distance % (1000 * 60)) / 1000)).slice(-2);
-            $($(element).find(".hours .value")).text(hours);
-            $($(element).find(".minutes .value")).text(minutes);
-            $($(element).find(".seconds .value")).text(seconds);
-        }
-        else {
-            // Reveal the image
-            showTime(element);
-            $(element.parentElement.parentElement).removeClass("blur-image");
-            $(element).next().show();
-            $(element).remove();
-            clearInterval(counter);
-        }
-    }, 1000);
+  var countdownDate = new Date("Apr 15, 2024 00:00:00").getTime() + $(element).attr("hours") * 60 * 60 * 1000;
+  var counter = setInterval(() => {
+    var now = new Date().getTime();
+    var distance = countdownDate - now;
+    if (distance > 0) {
+      var hours = ("0" + Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).slice(-2);
+      var minutes = ("0" + Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).slice(-2);
+      var seconds = ("0" + Math.floor((distance % (1000 * 60)) / 1000)).slice(-2);
+      $($(element).find(".hours .value")).text(hours);
+      $($(element).find(".minutes .value")).text(minutes);
+      $($(element).find(".seconds .value")).text(seconds);
+    }
+    else {
+      // Reveal the image
+      showTime(element);
+      $(element.parentElement.parentElement).removeClass("blur-image");
+      $(element).next().show();
+      $(element).remove();
+      clearInterval(counter);
+    }
+  }, 1000);
 });
 
 const balloonContainer = document.getElementById("balloon-container");
@@ -47,11 +47,11 @@ function getRandomStyles() {
 function createBalloons(num) {
   for (var i = num; i > 0; i--) {
     setTimeout(() => {
-        var balloon = document.createElement("div");
-        balloon.className = "balloon";
-        balloon.style.cssText = getRandomStyles();
-        balloonContainer.append(balloon);
-    }, 500*i);
+      var balloon = document.createElement("div");
+      balloon.className = "balloon";
+      balloon.style.cssText = getRandomStyles();
+      balloonContainer.append(balloon);
+    }, 500 * i);
   }
 }
 
@@ -59,15 +59,9 @@ window.addEventListener("load", () => {
   createBalloons(10);
 });
 
-$("#carouselExample").swiperight(function() {
-    $(this).carousel('prev');
-  });
- $("#carouselExample").swipeleft(function() {
-    $(this).carousel('next');
- });
-
-function showTime(element)
-{
-	var curtain = element.parentElement.parentElement.firstElementChild;
-	$(curtain).addClass("open");
+function showTime(element) {
+  var curtain = element.parentElement.parentElement.firstElementChild;
+  $(curtain).addClass("open");
 }
+
+$('.carousel-control-prev').focus();
